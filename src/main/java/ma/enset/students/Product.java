265b57c44d7fac11b86aa37681131291aba15d0a
nadabettach <nadabettach6@gmail.com>
2025-04-06 15:45:19 +0100

    creation de la classe Product

--- src/main/java/ma/enset/students/Product.java
+++ src/main/java/ma/enset/students/Product.java
@@ -1,4 +1,20 @@
 package ma.enset.students;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id; // <-- CORRECT IMPORT
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
 public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+    private int quantity;
 }
+
--- src/main/java/ma/enset/students/StudentsApplication.java
+++ src/main/java/ma/enset/students/StudentsApplication.java
@@ -1,13 +1,18 @@
 package ma.enset.students;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentsApplication {
+public class StudentsApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentsApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
